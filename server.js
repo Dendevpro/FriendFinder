ee816d4925619541aa361213b25e968f6e54f71a
Dendevpro <mauro.facciolla@gmail.com>
2019-03-12 12:52:53 -0700

    Styling

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ require("./app/routing/htmlRoutes.js")(app);
 
 // LISTENER
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on: http://localhost:" + PORT);
 });
